3e2a7a2f2e855b5ffdbb9ba16f9e143767fe2ca5
Artem Bahmet <baget.masta@gmail.com>
2023-08-09 19:25:57 +0300

    fill server.js and app.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require("./app");
+
+const mongoose = require("mongoose");
+
+const { DB_HOST, PORT = 4000 } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    return app.listen(PORT);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
