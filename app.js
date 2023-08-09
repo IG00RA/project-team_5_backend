5043374164076b26d53b9c9a8c9aa0ae1cd52f6b
Artem Bahmet <baget.masta@gmail.com>
2023-08-09 22:11:06 +0300

    dotenv to the srver

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const app = require("./app");
 
 const mongoose = require("mongoose");
