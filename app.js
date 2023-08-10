c34e4b83310d51aa85a493bdd23615ff6b8dfe30
Kateryna Verveda <katushka211@gmail.com>
2023-08-10 18:17:07 +0200

    add tasksRouter

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const httpMessage = require("./constants");
 
 const authRouter = require("./routes/api/auth");
+const { tasksRouter } = require("./routes/api/tasks");
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
+app.use("/api/tasks", tasksRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: httpMessage[404] });
