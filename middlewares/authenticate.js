7cf7e98b06e62d012897af35b5c7a60c6df7d6fb
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-08-10 23:37:48 +0300

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken");
-const HttpError = require("../helpers/HttpError");
-require("dotenv").config();
 const { SECRET_KEY } = process.env;
 const User = require("../models/User");
+const { HttpError } = require("../helpers");
 
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
