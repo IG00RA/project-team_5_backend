79edb75f6aeb1ba8102bc6d1e5c210d3a1a1026f
Kateryna Verveda <katushka211@gmail.com>
2023-08-10 18:28:13 +0200

    export middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,5 @@
-const validateBody = require("./validateBody");
+const { validateBody } = require("./validateBody");
+const { isValidId } = require("./isValidId");
+const { authenticate } = require("./authenticate");
 
-module.exports = { validateBody };
+module.exports = { validateBody, isValidId, authenticate };
