85edda7a695ac2f861fcaf79698dba2ee3532d39
Kateryna Verveda <katushka211@gmail.com>
2023-08-15 17:25:32 +0200

    add error message in middleware validateTime

--- middlewares/validateTime.js
+++ middlewares/validateTime.js
@@ -1,11 +1,16 @@
 const moment = require("moment");
+const { HttpError } = require("../helpers");
 
 const validateTime = (schema) => {
   try {
     schema.path("end").validate(function (value) {
       const startMoment = moment(this.start, "HH:mm");
       const endMoment = moment(value, "HH:mm");
-      return endMoment.isAfter(startMoment);
+      const validate = endMoment.isAfter(startMoment);
+      if (!validate) {
+        throw new HttpError(409, "Cannot be earlier than start");
+      }
+      return validate;
     });
   } catch (error) {
     console.log(error.message);
