235e60ea80acd132e36e50082a34b43b4e0e9e55
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-08-21 22:40:07 +0300

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -4,7 +4,7 @@ const Joi = require("joi");
 const { validateTime } = require("../middlewares/validateTime");
 
 const timeRegexp = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
-
+//
 const tasksSchema = new Schema(
   {
     title: {
