71f84de332b32507a9f735deb03f6f3a7aff5c6a
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-08-21 17:06:21 +0300

    add joi start>end validate

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,10 +2,17 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   return (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-      next(new HttpError(400, "Invalid request body"));
+    const validationResult = schema.validate(req.body);
+
+    if (validationResult.error) {
+      const errorMessages = validationResult.error.details.map(
+        (detail) => detail.message
+      );
+      const errorMessage = errorMessages.join(", ");
+      return next(new HttpError(400, `${errorMessage}`));
+      // return res.status(400).json({ error: errorMessage });
     }
+
     next();
   };
 };
--- models/Task.js
+++ models/Task.js
@@ -4,6 +4,10 @@ const Joi = require("joi");
 const { validateTime } = require("../middlewares/validateTime");
 
 const timeRegexp = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+const dataRegexp = /^\d{4}-\d{2}-\d{2}$/;
+
+const priorityType = ["low", "medium", "high"];
+const categoryType = ["in-progress", "completed", "pending"];
 
 const tasksSchema = new Schema(
   {
@@ -50,17 +54,45 @@ const tasksSchema = new Schema(
   },
   { versionKey: false, timestamps: false, collection: "tasks" }
 );
-tasksSchema.post("save", handleMongooseError);
+
 tasksSchema.plugin(validateTime).post("save", handleMongooseError);
 
+const validateStartEndTime = (obj, helpers) => {
+  function toMinute(time) {
+    const arrTime = time.split(":");
+    return Number(arrTime[0]) * 60 + Number(arrTime[1]);
+  }
+  const { start, end } = obj;
+
+  if (toMinute(start) >= toMinute(end)) {
+    return helpers.error("any.invalid");
+  }
+};
+
 const addSchema = Joi.object({
-  title: Joi.string().min(3).max(250).required(),
-  start: Joi.string().regex(timeRegexp),
-  end: Joi.string().regex(timeRegexp),
-  priority: Joi.string().valid("low", "medium", "high"),
-  date: Joi.string().isoDate().required(),
-  category: Joi.string().required(),
-});
+  title: Joi.string().max(250).required(),
+  date: Joi.string().pattern(dataRegexp).min(10).max(10).required().messages({
+    "string.pattern.base": `The field "date" must be of the following type "YYYY-MM-DD"`,
+  }),
+  start: Joi.string().pattern(timeRegexp).min(5).max(5).required().messages({
+    "string.pattern.base": `The field "start" must be of the following type "hh:mm"`,
+  }),
+
+  end: Joi.string().pattern(timeRegexp).min(5).max(5).required().messages({
+    "string.pattern.base": `The field "end" must be of the following type "hh:mm"`,
+  }),
+  priority: Joi.string()
+    .valid(...priorityType)
+    .required(),
+
+  category: Joi.string()
+    .valid(...categoryType)
+    .required(),
+})
+  .custom(validateStartEndTime)
+  .messages({
+    "any.invalid": `The following condition must be met start<end`,
+  });
 
 const Task = model("task", tasksSchema);
 
