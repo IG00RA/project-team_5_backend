00c3da030a0c28cb8ecd088990c8b8f9e32cabd7
Igor Gromadskiy <113199658+IG00RA@users.noreply.github.com>
2023-08-22 15:51:42 +0300

    Delete regex.js

--- constants/regex.js
+++
@@ -1,5 +0,0 @@
-const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-const birthdayRegexp =
-  /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-
-module.exports = { emailRegexp, birthdayRegexp };
