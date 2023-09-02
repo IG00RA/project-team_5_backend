ad409e46e3d34a9ad9b99045cd1f5ad549f55006
Kateryna Verveda <katushka211@gmail.com>
2023-09-02 20:08:31 +0200

    update validation for review

--- models/Review.js
+++ models/Review.js
@@ -25,8 +25,17 @@ const reviewsSchema = new Schema(
 reviewsSchema.post("save", handleMongooseError);
 
 const addReviewSchema = Joi.object({
-  raiting: Joi.string().valid("1", "2", "3", "4", "5"),
-  review: Joi.string().min(5).max(300),
+  raiting: Joi.string().required().valid("1", "2", "3", "4", "5").messages({
+    "string.empty": "Rating is required",
+    "any.only": "Rating must be one of '1', '2', '3', '4', or '5'",
+    "any.required": "Rating is required",
+  }),
+  review: Joi.string().min(5).max(300).required().messages({
+    "string.empty": "Review is required",
+    "string.min": "Review must be at least 5 characters",
+    "string.max": "Review must not exceed 300 characters",
+    "any.required": "Review is required",
+  }),
 });
 
 const Review = model("review", reviewsSchema);
